e02367fa50e714afc201871941ed097972b2a08c
seth <sethj@centerofthewest.org>
2014-09-13 17:02:03 +0000

    ayah fix

---
+++ webroot/ayah_config_sample.php
@@ -0,0 +1,6 @@
+<?php
+//you need to get your own publisher and scoring key if you plan to use this
+define( 'AYAH_PUBLISHER_KEY', "");
+define( 'AYAH_SCORING_KEY', '');
+define( 'AYAH_WEB_SERVICE_HOST', "ws.areyouahuman.com");
+
