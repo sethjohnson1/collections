25e30c2541c61aca5788cbd7193a6a8d29bd81e4
Lloyd Johnson <lloydj@centerofthewest.org>
2014-10-23 16:17:24 +0000

    removed layout

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -24,7 +24,7 @@ class AppController extends Controller {
 				//ask LLoyd what variable would be useful, or just some flag?
 				//also write to DB?
 				//debug('9+ pageviews!');
-				$this->Cookie->write('show_it',1,false, '12 months');
+				//$this->Cookie->write('show_it',1,false, '12 months');
 			}
 			else $this->Cookie->write('page_counter',$count, false, '5 days');
 			
@@ -34,7 +34,7 @@ class AppController extends Controller {
 			if (!empty($timer)){
 				$minutes=(time()-$timer)/60;
 				if ($minutes > 10){
-					$this->Cookie->write('show_it',1,false, '12 months');
+					//$this->Cookie->write('show_it',1,false, '12 months');
 				}
 			}
 			else {
