128e490392050f829ae18967a48d884cf8a9c241
Seth J Home <seth@sethjohnson.net>
2016-02-24 05:02:55 +0000

    indices SQL script

---
+++ add_indices.sql
@@ -0,0 +1,10 @@
+alter table makers_treasures add index (maker_id);
+alter table makers_treasures add index (treasure_id);
+alter table tags_treasures add index (tag_id);
+alter table tags_treasures add index (treasure_id);
+alter table treasures_medvalues add index (medvalue_id);
+alter table treasures_medvalues add index (treasure_id);
+alter table treasures_usergals add index (treasure_id);
+alter table treasures_usergals add index (usergal_id);
+alter table comments_users add index (user_id);
+alter table comments_users add index (comment_id);
