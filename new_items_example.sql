093842c5a8797f72cdd9dce4cbe15f0e47f68f30
Seth J Home <seth@sethjohnson.net>
2016-02-24 14:17:05 +0000

    stuff

--- new_items_example.sql
+++ new_items_example.sql
@@ -3,6 +3,16 @@ this is an example of how to populate a virtual gallery of new items, assuming y
 
  183 is the id of the virtual gallery! I usually make a new, blank one, and then run this */
 
+ INSERT INTO `usergals` (`name`, `creator`, `img`, `email`, `editcode`, `user_id`, `gloss`, `flagged`, `listed`) VALUES
+('New Items',	'Seth J',	'nh.304.75.2.jpg',	'sethj@centerofthewest.org',	'ree61ujg',	'sethj@centerofthewest.org',	'Latest items harvested from our collections database!',	NULL,	1);
+ 
 insert into treasures_usergals (treasure_id, usergal_id, created)
-select id, 183, now()
+select id, 216, now()
 from treasures_new;
+
+
+/* now to get homeflags from other DB */
+
+update oc20.treasures, oc18.treasures
+set oc20.treasures.homeflag=oc18.treasures.homeflag
+where oc20.treasures.id=oc18.treasures.id;
